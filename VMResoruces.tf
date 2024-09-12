f5bedd0cbeffc6c1df2573f66b9f6d48e1d322b7
pawantiwari1814 <pawan.a.kumar.tiwari@accenture.com>
2024-09-12 15:37:26 +0530

    Update VMResoruces.tf

--- VMResoruces.tf
+++ VMResoruces.tf
@@ -1,8 +1,8 @@
-resource "azurerm_virtual_machine" "vmpawan" {
+resource "azurerm_virtual_machine" "vmpawan1" {
   name                  = "testetstetestvm"
   resource_group_name   = "TerraformRG"
   location              = "East US"
-  network_interface_ids = [azurerm_network_interface.pawannic1.id]
+  network_interface_ids = [azurerm_network_interface.nic1.id]
   vm_size               = "Standard_DS1_v2"
   storage_os_disk {
     name = "vishnudisk1"
@@ -36,7 +36,7 @@ resource "azurerm_virtual_machine" "vmpawan" {
   }
 }
 
-resource "azurerm_virtual_network" "Pawanvnet1" {
+resource "azurerm_virtual_network" "vnet1" {
   name                = "testetstetest"
   address_space       = ["10.0.0.0/16"]
   location            = "East US"
@@ -44,21 +44,21 @@ resource "azurerm_virtual_network" "Pawanvnet1" {
 }
 
 
-resource "azurerm_subnet" "pawansubnet1" {
+resource "azurerm_subnet" "subnet1" {
   name                 = "hrmante"
   resource_group_name  = "TerraformRG"
-  virtual_network_name = azurerm_virtual_network.Pawanvnet1.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
   address_prefixes     = ["10.0.2.0/24"]
 }
 
-resource "azurerm_network_interface" "pawannic1" {
+resource "azurerm_network_interface" "nic1" {
   name                = "vishnunic111"
   location            = "East US"
   resource_group_name = "TerraformRG"
 
   ip_configuration {
     name                          = "config111"
-    subnet_id                     = azurerm_subnet.pawansubnet1.id
+    subnet_id                     = azurerm_subnet.subnet1.id
     private_ip_address_allocation = "Dynamic"
     public_ip_address_id =azurerm_public_ip.public1.id
   }
